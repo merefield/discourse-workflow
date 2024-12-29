a6082ead93226b854240a8c55d859e84de5f3092
merefield <merefield@gmail.com>
2024-12-29 14:04:15 +0000

    add id to step options so they can be ordered

--- db/migrate/20241207161112_create_workflow_step_options.rb
+++ db/migrate/20241207161112_create_workflow_step_options.rb
@@ -2,6 +2,7 @@
 class CreateWorkflowStepOptions < ActiveRecord::Migration[7.1]
   def change
     create_table :workflow_step_options do |t|
+      t.integer :workflow_step_option_id
       t.references :workflow_step, foreign_key: true
       t.references :workflow_option, foreign_key: true
       t.integer :target_step_id
