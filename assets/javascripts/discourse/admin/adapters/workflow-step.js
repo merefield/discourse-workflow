0740baed363fedeaf1895d1f42064aa0f6b0138d
merefield <merefield@gmail.com>
2024-12-31 19:04:22 +0000

    fix update record for steps

--- assets/javascripts/discourse/admin/adapters/workflow-step.js
+++ assets/javascripts/discourse/admin/adapters/workflow-step.js
@@ -4,14 +4,11 @@ export default class Adapter extends RestAdapter {
   jsonMode = true;
 
   basePath(store, type, findArgs) {
-    debugger;
-
     if (findArgs) {
       if (typeof findArgs === "object") {
         return `/admin/plugins/discourse-workflow/workflows/${findArgs.workflow_id}/`
       } else {
         return `/admin/plugins/discourse-workflow/`
-        // return `/admin/plugins/discourse-workflow/workflows/${findArgs}/`
       }
     } else {
       return "/admin/plugins/discourse-workflow/";
@@ -19,17 +16,14 @@ export default class Adapter extends RestAdapter {
   }
 
   pathFor(store, type, findArgs) {
-    debugger;
     // removes underscores which are implemented in base
     let path =
       this.basePath(store, type, findArgs) +
       store.pluralize(this.apiNameFor(type));
-      return path;
-    // return path  this.appendQueryParams(path, findArgs);
+    return this.appendQueryParams(path, findArgs);
   }
 
   apiNameFor() {
-    // debugger;
     return "workflow_step";
   }
 }
