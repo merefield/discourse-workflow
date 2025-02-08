1646f9978dcc1810061ac0afb5b743fc4508c510
merefield <merefield@gmail.com>
2025-02-08 14:12:16 +0000

    bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-workflow
 # about: A topic-based workflow engine for Discourse
-# version: 0.0.5
+# version: 0.0.6
 # authors: Robert Barrow
 # contact_emails: robert@pavilion.tech
 # url: https://github.com/merefield/discourse-workflow
