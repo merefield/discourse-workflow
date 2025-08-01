8e376e03712602fca2d18ea9b2fd56c53471796a
merefield <merefield@gmail.com>
2025-08-01 22:08:28 +0100

    linting

--- db/migrate/20250530102741_create_workflow_stats.rb
+++ db/migrate/20250530102741_create_workflow_stats.rb
@@ -1,5 +1,6 @@
 
 # frozen_string_literal: true
+# rubocop:disable Discourse/NoAddReferenceOrAliasesActiveRecordMigration
 class CreateWorkflowStats < ActiveRecord::Migration[7.2]
   def change
     create_table :workflow_stats do |t|
--- lib/discourse_workflow/stats.rb
+++ lib/discourse_workflow/stats.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module DiscourseWorkflow
   class Stats
 
@@ -11,9 +12,9 @@ module DiscourseWorkflow
       Workflow.all.each do |workflow|
         workflow.workflow_state.each do |state|
           stat = ::DiscourseWorkflow::WorkflowStat.find_or_initialize_by(
-            cob_date:          current_date,
-            workflow_id:       workflow.id,
-            workflow_step_id:  state.workflow_step_id
+            cob_date: current_date,
+            workflow_id: workflow.id,
+            workflow_step_id: state.workflow_step_id
           )
           stat.count ||= 0
           stat.count += 1
