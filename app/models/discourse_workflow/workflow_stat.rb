f266bfde9aadbf23ddab8b8adfe766b71dfa161a
merefield <merefield@gmail.com>
2025-08-01 21:57:39 +0100

    annotate

--- app/models/discourse_workflow/workflow_stat.rb
+++ app/models/discourse_workflow/workflow_stat.rb
@@ -11,3 +11,26 @@ module ::DiscourseWorkflow
     validates :count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   end
 end
+
+# == Schema Information
+#
+# Table name: workflow_stats
+#
+#  id               :bigint           not null, primary key
+#  cob_date         :datetime
+#  count            :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  workflow_id      :bigint
+#  workflow_step_id :bigint
+#
+# Indexes
+#
+#  index_workflow_stats_on_workflow_id       (workflow_id)
+#  index_workflow_stats_on_workflow_step_id  (workflow_step_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (workflow_id => workflows.id)
+#  fk_rails_...  (workflow_step_id => workflow_steps.id)
+#
